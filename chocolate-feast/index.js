86d956c17bacdc9ee605db968c0e951c93d4522a
Rodrigo Faria <rodrigolfsi@gmail.com>
2020-09-02 13:21:13 -0300

    Chocolate Feast

---
+++ chocolate-feast/index.js
@@ -0,0 +1,24 @@
+function chocolateFeast(n, c, m) {
+  //n => money
+  //c => cost
+  //m => number of wrappers to change
+  let quantity = Math.floor(n / c)
+  let total = quantity
+
+  do {
+
+    let aux = Math.floor(quantity / m)
+    total += aux
+
+    quantity = aux + (quantity % m)
+
+    if (quantity < m) {
+      break;
+    }
+  } while(true)
+  
+  return total
+}
+
+t = chocolateFeast(6, 2, 2)
+console.log(t)
